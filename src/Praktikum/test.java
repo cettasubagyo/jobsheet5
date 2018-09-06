879663ee9a13312f5fe6bf86078e00ebdc10c99f
CRex64 <CRex64@3DWoRK>
2018-09-06 11:12:52 +0700

    mari beh

---
+++ src/Praktikum/test.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Praktikum;
+
+/**
+ *
+ * @author CRex64
+ */
+public class test {
+    public static void main(String[]args){
+        try{
+            ThrowThrows1.error();
+        }catch(Exception a){
+            a.printStackTrace();
+        }
+    }
+}
---
+++ src/jobsheet5/NullPointerDemo.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jobsheet5;
+
+/**
+ *
+ * @author CRex64
+ */
+public class NullPointerDemo {
+    public static void main(String[] args) {
+       String name=null;
+       System.out.print("Length of the string"+ name.length());
+    }
+}
